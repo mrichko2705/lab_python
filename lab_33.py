ed27503e614846f335d545e5d911a89f2914fb98
Misha <32159444+mrichko2705@users.noreply.github.com>
2017-09-21 15:47:36 +0300

    Add files via upload

---
+++ lab_33.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+weight = float(input("Ваша вага = "))
+height = float(input("Ваш зріст = "))
+BMI = weight/(height**2)
+print("Індекс Маси Вашого Тіла = ", BMI)

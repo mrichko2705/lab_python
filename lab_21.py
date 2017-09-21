f20d61bf9b46963c35c14ecdcaaa15e6ed6934fe
Misha <32159444+mrichko2705@users.noreply.github.com>
2017-09-21 15:09:19 +0300

    Add files via upload

---
+++ lab_21.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+print(" Привіт {} {} {} ".format(sys.argv[1],sys.argv[2],sys.argv[3]))
---
+++ lab_22.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+a,b,c = input("Ваше прізвище, ім'я, по-батькові: ").split(sep = ' ')
+print(" Привіт {} {} {} ".format(a,b,c))

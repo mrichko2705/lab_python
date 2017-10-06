9248d522a030e44183c4e132440e844291936653
Misha <32159444+mrichko2705@users.noreply.github.com>
2017-10-07 00:00:45 +0300

    Add files via upload

---
+++ py_76.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# --*-- coding:utf-8 --*--
+
+input_srt = input("Введіть рядок: ")  
+print("**" + "*"*len(input_srt) + "**\n" + "* " + input_srt + " *\n" + "**" + "*"*len(input_srt) + "**")
+
+	
